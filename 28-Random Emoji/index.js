710562b6983d7e4b923055f9f38983eabf25f8f3
laxman67 <laxmanhenricks94@gmail.com>
2023-10-08 23:15:54 +0530

    28 -Making Feedback UI Project

---
+++ 28-Random Emoji/index.js
@@ -0,0 +1,30 @@
+const btnEl = document.getElementById("btn");
+const emojiNameEL = document.getElementById("emoji-name");
+const API_KEY = "0ec4807294677600a591a4de917e2ddca16789f2";
+
+//
+const emoji = [];
+
+async function getEmoji() {
+  let response = await fetch(
+    "https://emoji-api.com/emojis?access_key=0ec4807294677600a591a4de917e2ddca16789f2"
+  );
+
+  data = await response.json();
+
+  for (let index = 0; index < 1500; index++) {
+    emoji.push({
+      emojiName: data[index].character,
+      emojiCode: data[index].unicodeName,
+    });
+  }
+}
+
+getEmoji();
+
+btnEl.addEventListener("click", () => {
+  randomNum = Math.floor(Math.random() * 1500);
+
+  btnEl.innerText = emoji[randomNum].emojiName;
+  emojiNameEL.innerText = emoji[randomNum].emojiCode;
+});
