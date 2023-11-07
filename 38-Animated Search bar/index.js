88bc8844d8b4bdcfcf9965823ab3ef5fda6b5f3b
laxman67 <laxmanhenricks94@gmail.com>
2023-11-07 21:15:43 +0530

    Animated Search Bar

---
+++ 38-Animated Search bar/index.js
@@ -0,0 +1,7 @@
+const searchBarContainer= document.querySelector(".search-bar-container")
+const magnifierEl= document.querySelector(".magnifier")
+
+magnifierEl.addEventListener("click",()=>{
+    searchBarContainer.classList.toggle("active")
+
+});
